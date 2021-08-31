491a6c4ab11f2e43d977068ef3cdfdd640b18e66
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-08-31 11:36:10 +0530

    notes

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var note = document.querySelector(".notes");
 
-var edit = document.querySelector(".edit");
-var delete = document.querySelector(".delete");
-var color = document.querySelector(".color");
+var editbtn = document.querySelector(".edit");
+var deletebtn = document.querySelector(".delete");
+var colorbtn = document.querySelector(".color");
 
 var main = document.querySelector(".main");
 var text = document.querySelector("textarea");
 
-edit.addEventListener('click', () => {
+editbtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
     text.classList.toggle('hidden');
+});
+
+text.addEventListener('input', (e) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
 });
