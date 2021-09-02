61ed5cbb13a2a62a28eceffa57ea378d11611c12
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-09-02 20:26:29 +0530

    notes

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-var note = document.querySelector(".notes");
+var addbtn = document.getElementById("add");
 
-var editbtn = document.querySelector(".edit");
-var deletebtn = document.querySelector(".delete");
-var colorbtn = document.querySelector(".color");
-
-var main = document.querySelector(".main");
-var text = document.querySelector("textarea");
-
-editbtn.addEventListener('click', () => {
-    main.classList.toggle('hidden');
-    text.classList.toggle('hidden');
+addbtn.addEventListener('click', () => {
+    addnewnote();
 });
 
-text.addEventListener('input', (e) => {
-    const { value } = e.target;
-    main.innerHTML = marked(value);
-    main.style.color = '#bdfcf9';
-    main.style.fontSize = '35px';
-    main.style.padding = '15px';
-    main.style.fontFamily = 'Patrick Hand, cursive';
-    main.style.letterSpacing = '2px';
-});
+function addnewnote() {
+    var notes = document.createElement('div');
+    notes.classList.add('notes');
 
-{/* <div class="notes">
-    <div class="tools">
-        <button class="edit"><i class="fas fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash"></i></button>
-        <button class="color"><i class="fas fa-palette"></i></button>
-    </div>
-    <div class="main hidden">
-    </div>
-    <textarea></textarea>
-</div> */}
+    notes.innerHTML = 
+    `<div class="notes">
+        <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash"></i></button>
+            <button class="color"><i class="fas fa-palette"></i></button>
+        </div>
+        <div class="main hidden">
+        </div>
+        <textarea></textarea>
+    </div> `;
+
+    var editbtn = notes.querySelector(".edit");
+    var deletebtn = notes.querySelector(".delete");
+    var colorbtn = notes.querySelector(".color");
+
+    var main = notes.querySelector(".main");
+    var text = notes.querySelector("textarea");
+
+    editbtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        text.classList.toggle('hidden');
+    });
+
+    deletebtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        text.classList.toggle('hidden');
+    });
+
+
+
+    text.addEventListener('input', (e) => {
+        const { value } = e.target;
+        main.innerHTML = marked(value);
+        main.style.color = '#bdfcf9';
+        main.style.fontSize = '35px';
+        main.style.padding = '15px';
+        main.style.fontFamily = 'Patrick Hand, cursive';
+        main.style.letterSpacing = '2px';
+    });
+
+    document.body.appendChild('notes');
+}
+
+
+
